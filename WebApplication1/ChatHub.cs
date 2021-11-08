a76a551062e830fa168a2f866490e55ba91cf150
Артём Зуев <Devil061@mail.ru>
2021-11-08 19:03:03 +0300

    Добавьте файлы проекта.

---
+++ WebApplication1/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class ChatHub:Hub
+    {
+        public async Task Send()
+        {
+            
+            await Clients.Others.SendAsync("Bro", $"{Context.User.Identity.Name}");
+        }
+        public async Task SendSis()
+        {
+            await Clients.Others.SendAsync("Sis", $"{Context.User.Identity.Name}");
+        }
+    }
+}
